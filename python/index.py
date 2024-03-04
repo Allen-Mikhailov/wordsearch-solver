9ffa51fa51868b09e7fe422268d886b8b507f90a
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-03-04 00:08:49 -0600

    Added some more informational output

--- python/index.py
+++ python/index.py
@@ -6,6 +6,9 @@ board = []
 for line in boardLines:
     board.append([*line.lower()])
 words = wordsf.read().split("\n")
+foundWords = {}
+for word in words:
+    foundWords[word] = False
 
 width = len(board[0])
 height = len(board)
@@ -38,8 +41,21 @@ def checkCharacter(x, y):
     for dir in directions:
         for word in words:
             if (validCheck(word, dir, x, y)):
+                foundWords[word] = True
                 print(word+" found at "+str(x)+", "+str(y)+" in dir "+str(dir))
 
 for x in range(width):
     for y in range(height):
         checkCharacter(x, y)
+
+foundCount = 0
+for word in words:
+    print(word+" "+ ("found" if foundWords[word] else "not found"))
+    if (foundWords[word]):
+        foundCount += 1
+print('Found '+str(foundCount) + " of "+str(len(words)) + " words")
+
+string = ""
+for word in words:
+    string+=word+","
+print(string)
